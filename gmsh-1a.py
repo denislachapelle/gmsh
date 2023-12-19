41cec295e554c5931b7ecb38444dc852f96e28df
denislachapelle <45741871+denislachapelle@users.noreply.github.com>
2023-12-19 07:00:43 -0500

    Add python script using gmsh, gmsh-1a.py

---
+++ gmsh-1a.py
@@ -0,0 +1,63 @@
+# ------------------------------------------------------------------------------
+#
+#  Gmsh Python tutorial 1
+#
+#  Geometry basics, elementary entities, physical groups
+#
+# ------------------------------------------------------------------------------
+
+
+import gmsh
+import sys
+
+gmsh.initialize()
+
+gmsh.model.add("gmsh-1")
+
+lc = 0.1
+#draw a rectangle.
+gmsh.model.occ.addPoint(0, 0, 0, lc, 1)
+gmsh.model.occ.addPoint(1, 0, 0, lc, 2)
+gmsh.model.occ.addPoint(1, 1, 0, lc, 3)
+gmsh.model.occ.addPoint(0, 1, 0, lc, 4)
+gmsh.model.occ.addLine(1, 2, 1)
+gmsh.model.occ.addLine(2, 3, 2)
+gmsh.model.occ.addLine(3, 4, 3)
+gmsh.model.occ.addLine(4, 1, 4)
+gmsh.model.occ.addCurveLoop([1, 2, 3, 4], 1)
+#draw a circle.
+gmsh.model.occ.addCircle(0.5, 0.5, 0.0, 0.1, 5) 
+gmsh.model.occ.addCurveLoop([5], 2)
+
+#draw a circle.
+gmsh.model.occ.addCircle(0.25, 0.25, 0.0, 0.1, 6) 
+gmsh.model.occ.addCurveLoop([6], 3)
+#add a surface
+gmsh.model.occ.addPlaneSurface([1, 2, 3], 1)
+
+gmsh.model.occ.synchronize()
+#gmsh.model.occ.synchronize()
+
+gmsh.model.mesh.setTransfiniteCurve(5, 72)
+gmsh.model.mesh.setTransfiniteCurve(6, 72)
+
+#add a physical group.
+gmsh.model.addPhysicalGroup(2, [1], 1, name="copper")
+gmsh.model.addPhysicalGroup(1, [1], 2, name="ground")
+gmsh.model.addPhysicalGroup(1, [3], 3, name="onevolt")
+gmsh.model.addPhysicalGroup(1, [2, 4], 4, name="zeroflux")
+
+# We can then generate a 2D mesh...
+gmsh.model.mesh.generate(2)
+
+#gmsh.option.setNumber("Mesh.MshFileVersion", 1)
+
+# ... and save it to disk
+gmsh.write("gmsh-1a.msh")
+
+# start gmsh
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
+
+#before leaving.
+gmsh.finalize()
